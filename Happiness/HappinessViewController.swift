0fe79454fe85686f9f12e58b1e252831d05efcfb
Tomek Micha≈Ç Pluci≈Ñski <darkarconio@hotmail.com>
2015-09-30 14:47:15 -0600

    Initial Commit, drawing smiley face

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Michal Plucinski on 2015-09-29.
+//  Copyright © 2015 Stanford. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+    
+}
